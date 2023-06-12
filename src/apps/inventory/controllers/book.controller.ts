039406b6699bcd4bcc7dbcc9ccbee369924f1feb
Bui Phu Cuong <cuong.buiphu@grevo.net>
2023-06-12 09:53:38 +0700

    inventory: detail

--- src/apps/inventory/controllers/book.controller.ts
+++ src/apps/inventory/controllers/book.controller.ts
@@ -1,7 +1,14 @@
 import { BookCreateDto } from '@apps/inventory/dtos/book/create.dto';
 import { BookService } from '@apps/inventory/services/book.service';
 import { UserRole } from '@shared/type';
-import { Authorized, Body, JsonController, Post } from 'routing-controllers';
+import {
+  Authorized,
+  Body,
+  Get,
+  JsonController,
+  Param,
+  Post,
+} from 'routing-controllers';
 import { Service } from 'typedi';
 
 @Service()
@@ -13,5 +20,11 @@ export class BookController {
   @Authorized(UserRole.MANAGER)
   async create(@Body() data: BookCreateDto) {
     return this.bookService.createGet(data);
+  }
+
+  @Get('/detail/:id')
+  @Authorized([UserRole.MANAGER])
+  async detail(@Param('id') id: string) {
+    return this.bookService.get(id);
   }
 }
--- src/apps/inventory/services/book.service.ts
+++ src/apps/inventory/services/book.service.ts
@@ -1,4 +1,6 @@
+import { Errors } from '@apps/inventory/configs/errors';
 import { BookRepository } from '@apps/inventory/repositories/book.repository';
+import { AppException } from '@shared/libs/exception';
 import { ResourceDataOutput, ResourceOuput } from '@shared/type';
 import { Service } from 'typedi';
 
@@ -37,6 +39,28 @@ export class BookService {
         stock: entity.stock,
         reorderThreshold: entity.reorderThreshold,
         stopOrder: entity.stopOrder,
+      },
+    };
+  }
+
+  async get(id: string): Promise<ResourceDataOutput<BookOutput>> {
+    const result = await this.bookRepository.get(id);
+    if (!result) {
+      const { errorCode, message, httpCode } = Errors.BOOK_NOT_FOUND;
+      throw new AppException(errorCode, message, httpCode);
+    }
+
+    return {
+      data: {
+        id: result.id,
+        title: result.title,
+        author: result.author,
+        price: result.price,
+        stock: result.stock,
+        reorderThreshold: result.reorderThreshold,
+        stopOrder: result.stopOrder,
+        createdAt: result.createdAt,
+        updatedAt: result.updatedAt,
       },
     };
   }
